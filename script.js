559ead8833be2c41a818e8cc6b0bafa1e01b35e3
Nikola Lukic <nlukic97@gmail.com>
2021-11-02 13:33:38 +0100

    drag fully functional

--- script.js
+++ script.js
@@ -8,58 +8,72 @@ document.addEventListener("drag", function(event) {
 
 
 document.addEventListener("dragstart", function(event) {
-  // store a ref. on the dragged elem
   dragged = event.target;
-  // make it half transparent
-  event.target.style.opacity = .5;
-  event.target.style.border = '1px solid rgba(0,0,0,0.5)';
+  event.target.classList.add('dragging')
 }, false);
 
 
 
 document.addEventListener("dragend", function(event) {
-    // reset the transparency
-    event.target.style.opacity = "";
-    event.target.style.border = '';
+  // reset the transparency
+  event.target.classList.remove('dragging')
+  // dragged = null
 }, false);
 
 
+// when we drag over each potential dropzone with a dragable element
+document.querySelectorAll('.dropzone').forEach(container=>{
+  container.addEventListener('dragover',(event)=>{
+    event.preventDefault()
+    const afterElement = getDragAfterElement(container,event.clientY)
 
-/* events fired on the drop targets */
-document.addEventListener("dragover", function(event) {
-  // prevent default to allow drop
-  event.preventDefault();
-}, false);
+    if(afterElement == null){
+      container.appendChild(dragged)
+    } else {
+      container.insertBefore(dragged, afterElement)
+    }
+  })
+})
+
+
+function getDragAfterElement(container,y){
+  let draggableElements = [... container.querySelectorAll('.draggable:not(.dragging)')]
+
+  // checking each element not being dragged to see where we have chosen to drop out dragged element
+  return draggableElements.reduce((closest, child)=>{
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2 //center of each box
+
+    if(offset < 0 && offset > closest.offset){
+      return {offset: offset, element: child}
+    } else {
+      return closest
+    }
+  },{offset: Number.NEGATIVE_INFINITY}).element
+}
 
 
 
 document.addEventListener("dragenter", function(event) {
   // highlight potential drop target when the draggable element enters it
-  if (event.target.className == "dropzone") {
-    event.target.style.background = "purple";
-  }
-
+  if (event.target.className == "dropzone") event.target.style.background = "purple";
 }, false);
 
 
 
 document.addEventListener("dragleave", function(event) {
   // reset background of potential drop target when the draggable element leaves it
-  if (event.target.className == "dropzone") {
-    event.target.style.background = "";
-  }
-
+  if (event.target.className == "dropzone") event.target.style.background = "";
 }, false);
 
 
 
 document.addEventListener("drop", function(event) {
-  // prevent default action (open as link for some elements)
   event.preventDefault();
-  // move dragged elem to the selected drop target
+
   if (event.target.className == "dropzone") {
     event.target.style.background = "";
-    dragged.parentNode.removeChild( dragged );
-    event.target.appendChild( dragged );
+    // dragged.parentNode.removeChild(dragged);
+    // event.target.appendChild( dragged );
   }
 }, false);
